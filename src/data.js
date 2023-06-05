1897d591025b4f67d724d3058d9a6cf8e60fc695
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-06-05 14:29:23 -0400

    actualizacion2

--- src/data.js
+++ src/data.js
@@ -21,32 +21,21 @@ const result = getData();
 console.log(result);
 
 //Función que filtra la data por tipo de pokemon
-function filterType(result, typePokemon) {
+export function filterType(result, typePokemon) {
   return result.filter(item => item.type.includes(typePokemon));
 }
 
 const getPokemonTypeFire = filterType(result, "fire");
-
 const getPokemonTypeGrass = filterType(result, "grass");
-
 const getPokemonTypePoison = filterType(result, "poison");
-
 const getPokemonTypeWater = filterType(result, "water");
-
 const getPokemonTypeElectric = filterType(result, "electric");
-
 const getPokemonTypeFighting = filterType(result, "fighting");
-
 const getPokemonTypeFairy = filterType(result, "fairy");
-
 const getPokemonTypePsychic = filterType(result, "psychic");
-
 const getPokemonTypeIce = filterType(result, "ice");
-
 const getPokemonTypeRock = filterType(result, "rock");
-
 const getPokemonTypeFlying = filterType(result, "flying");
-
 const getPokemonTypeGround = filterType(result, "ground");
 
 console.log(getPokemonTypeFire);
@@ -63,18 +52,49 @@ console.log(getPokemonTypeFlying);
 console.log(getPokemonTypeGround);
 
 //con bucle For, logro iterar buscando los nombres de cada pokemon
-for (let i = 0; i < result.length; i++) {
-  console.log(result[i].name);
-  console.log(result[i].num);
-  console.log(result[i].about);
-  console.log(result[i].img);
+
+export function searchPokemon(result) {
+  let namePokemon = [];
+  const numPokemon = [];
+  const aboutPokemon = [];
+  const imgPokemon = [];
+
+  for (let i = 0; i < result.length; i++) {
+    namePokemon.push(result[i].name);
+    numPokemon.push(result[i].num);
+    aboutPokemon.push(result[i].about);
+    imgPokemon.push(result[i].img);
+
+  }
+  console.log(namePokemon)
 }
 
-//const namePokemon = result.filter(item => item.type.includes("name"));
-//const numPokemon = result.filter(item => item.type.includes ("num"));
-
-
-/*for (var i = 0; i < data.lenght; i++){
-  console.log(pokemon);
+export function sortData(result) {
+  result.sort((a, b) => {
+    if (a.name < b.name) { //a menor que b
+      return -1;
+    } else {
+      return 1;
+    };
+  });
+  return result;
 }
-*/
+console.log(sortData(result))
+
+/*export function dataReverse(result){
+  result.sort((a,b) => {
+    if(a.name < b.name){ //a mmayor que b
+    return 1; 
+    } 
+  }); 
+  return result;
+}
+console.log(dataReverse(result))*/
+export function dataalreves(result) {
+  result.sort((a, b) => {
+    if (a.name > b.name) { //a menor que b
+      return -1;
+    }
+  }
+  )
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,79 @@
 //import { data } from './pokemon.js';
 
-import {getData} from './data.js';
+import { getData } from './data.js';
 import data from './data/pokemon/pokemon.js';
-
+import { searchPokemon } from './data.js';
+import { filterType } from './data.js';
+import { sortData } from './data.js';
 //ACÁ VAN TODAS LAS INTERACCIONES CON EL DOM
+
+//const usersPokemon = document.getElementById("search_pokemon").addEventListener("click",(searchPokemon));
+
+//Función list Pokemmon, me ayuda a mostrar en mi div vacío, las características que quiero mostrar en las tarjetas de cada pokemon.
+function listPokemon(pokemons) {
+  for (let i = 0; i < pokemons.length; i++) { //el bucle For, va iterando por la data, rescatando nombre, numero, breve descripción e imagen.
+    const div = document.createElement("div")  //genero una etiqueta div, que será el "padre" y contenedor de mis etiquetas (img,h2,h3 y p)
+    div.id =pokemons[i].name;
+    div.onclick = function () {
+      // this.parentElement.removeChild(this);
+      viewModal(div.id)
+    };
+    div.classList.add("card");
+    // div.classList.add("container_card");
+    const imgPokemon = document.createElement("img"); 
+    imgPokemon.src = pokemons[i].img;
+    div.appendChild(imgPokemon);
+
+    const unpokemon = document.createElement("h2");
+    unpokemon.textContent = pokemons[i].name;
+    div.appendChild(unpokemon);
+   
+    const numPokemon = document.createElement("h3");
+    numPokemon.textContent = pokemons[i].num;
+    div.appendChild(numPokemon);
+
+    /*const typePokemon = document.createElement("p");
+    typePokemon.textContent = pokemons[i].type;
+    div.appendChild(typePokemon);*/
+    
+    const aboutPokemon = document.createElement("p");
+    aboutPokemon.textContent = pokemons[i].about;
+    div.appendChild(aboutPokemon);
+
+    document.getElementById("container_cards").appendChild(div);// le digo que vaya al documento y tome el ID "container cards" e inserte mi "div" creado en js.
+    
+
+  }
+}
+const pokemons = getData();
+listPokemon(pokemons);
+
+
+function viewModal(id){
+  // Get the modals
+  var modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  var btn = document.getElementById(id);
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+//ligo funciones con botones
+document.getElementById("filterType").addEventListener("click",(filterType));
