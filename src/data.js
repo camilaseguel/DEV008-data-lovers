930a1ab8da86004a060c532eda833fd6db3e0a0b
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-06-07 14:31:46 -0400

    cambiosmiercoles

--- src/data.js
+++ src/data.js
@@ -4,21 +4,24 @@ import data from './data/pokemon/pokemon.js';
 
 //ACÁ VAN TODAS LAS FUNCIONES PARA MANIPULAR DATOS
 
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
 //Función que toma la información de la data.
 export function getData() {
   return data.pokemon
 }
+
 //constante resultado, que hace referencia a mi función de tomar información de data.
 const result = getData();
 console.log(result);
+
+const copyresultDos = [...result];
+console.log(copyresultDos)
+
+const copyresult = [...result];
+console.log(copyresult)
+
+const copyResultOrder = [...result];
+console.log(copyResultOrder)
+
 
 //Función que filtra la data por tipo de pokemon
 export function filterType(result, typePokemon) {
@@ -37,29 +40,13 @@ const getPokemonTypeRock = filterType(result, "rock");
 const getPokemonTypeFlying = filterType(result, "flying");
 const getPokemonTypeGround = filterType(result, "ground");
 
-/*
-console.log(getPokemonTypeFire);
-console.log(getPokemonTypeGrass);
-console.log(getPokemonTypePoison);
-console.log(getPokemonTypeWater);
-console.log(getPokemonTypeElectric);
-console.log(getPokemonTypeFighting);
-console.log(getPokemonTypeFairy);
-console.log(getPokemonTypePsychic);
-console.log(getPokemonTypeIce);
-console.log(getPokemonTypeRock);
-console.log(getPokemonTypeFlying);
-console.log(getPokemonTypeGround);
-*/
 //Función que filtra la data por generación de pokemon
 export function filterGeneration(result, generationPokemon) {
-  return result.filter(item => item.generation.name==generationPokemon);
+  return result.filter(item => item.generation.name == generationPokemon);
 }
-const getPokemonGenerationI = filterGeneration(result,"kanto");
-const getPokemonGenerationII = filterGeneration(result,"johto");
+const getPokemonGenerationI = filterGeneration(result, "kanto");
+const getPokemonGenerationII = filterGeneration(result, "johto");
 
-console.log(getPokemonGenerationI);
-console.log(getPokemonGenerationII);
 
 //con bucle For, logro iterar buscando los nombres de cada pokemon
 
@@ -79,9 +66,9 @@ export function searchPokemon(result) {
   console.log(namePokemon)
 }
 
-//Función que ordena la data alfabéticamente (nombre)
-export function sortData(result) {
-  result.sort((a,b) => {
+//Función que ordena la data alfabéticamente a-z (nombre)
+export function orderNameAscending(result) {
+  result.sort((a, b) => {
     if (a.name < b.name) { //a menor que b
       return -1;
     }
@@ -92,31 +79,40 @@ export function sortData(result) {
   });
   return result;
 }
-// console.log(sortData(result));
+console.log(orderNameAscending(result));
 
-/*export function sortDataZa(result) {
-  result.sort((b,a) => {
-    if (a.name < b.name) { //a menor que b
+export function orderNameDescending(copyresultDos) {
+  copyresultDos.sort((a, b) => {
+    if (a.name > b.name) { //a menor que b
       return -1;
     }
-    if (a.name > b.name) {
+    if (a.name < b.name) {
       return 1;
     }
     return 0;
   });
-  return result;
+  return copyresultDos;
 }
+console.log(orderNameDescending(copyresultDos));
 
-console.log(result.sort((a,b) => a.name.localeCompare(b.name)));
-console.log(result.sort((a,b) => b.name.localeCompare(a.name)));
 
-function dataZa(result){
+const orderNumberAscending = copyresult.sort((a, b) => {
+  return a.num - b.num
+});
+console.log(orderNumberAscending)
+
+const orderNumbersDescending = copyResultOrder.sort((a, b) => {
+  return b.num - a.num
+});
+console.log(orderNumbersDescending)
+
+/*function dataZa(result){
   const dataDescending = result.reverse();
   return dataDescending;
 }
 console.log (dataZa(result));
 
-*/
+
 /*let keysArrays =Object.values(result);
 console.log(keysArrays)*/
 
